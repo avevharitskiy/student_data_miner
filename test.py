62086660e0d75c9f378b8bd8d594d0f977271d10
av.evharitskiy <evharitskiy96@gmail.com>
2019-06-19 22:38:53 +0300

    Remove trash

--- test.py
+++
@@ -1,6 +0,0 @@
-from graph_tool.all import *
-g = collection.data["football"]
-print(g)
-state = minimize_blockmodel_dl(g)
-pos = arf_layout(g)
-state.draw(pos=state.bfield, output="football-sbm-fit.svg")
